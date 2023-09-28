351f996577d1a049ddb434939ef863d7cae29a5a
CarlosDelMarmolFijo <93593998+CarlosDelMarmolFijo@users.noreply.github.com>
2023-09-28 19:39:42 +0200

    Add files via upload

---
+++ Horas de libre configuración/python/Carlos_Del_Marmol_Fijo_Practica_1.1.py
@@ -0,0 +1,14 @@
+# Generamos un fichero que ejecute las siguientes salidas:
+
+# Escribir como comentario el enunciado de la práctica.
+# Escribir el siguiente texto en tres líneas, pero con una sola sentencia Print.
+
+# En la ciudad de Pamplona hay una plaza. En la plaza hay una esquina.
+# En la esquina hay una casa. En la casa hay una pieza. En la pieza hay una cama. En la cama hay una estera. En la estera hay una barra.
+# En la barra hay una lora. Luego la lora a la barra, la barra a la estera, la estera a la cama, la cama a la pieza, la pieza a la casa, la casa a la esquina, la esquina a la plaza, la plaza a la ciudad de Pamplona.
+
+
+print('En la ciudad de Pamplona hay una plaza. En la plaza hay una esquina. \n'
+' En la esquina hay una casa. En la casa hay una pieza. En la pieza hay una cama. En la cama hay una estera. En la estera hay una barra. \n'
+' En la barra hay una lora. Luego la lora a la barra, la barra a la estera, la estera a la cama, la cama a la pieza, la pieza a la casa, ' 
+' la casa a la esquina, la esquina a la plaza, la plaza a la ciudad de \n Pamplona.')
