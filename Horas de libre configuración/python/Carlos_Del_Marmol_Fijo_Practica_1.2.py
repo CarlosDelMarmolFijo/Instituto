a677113478fa72c7fe62317dbc78ff7ca0a4fef6
CarlosDelMarmolFijo <93593998+CarlosDelMarmolFijo@users.noreply.github.com>
2023-09-28 19:35:19 +0200

    Delete Horas de libre configuración/python directory

--- Horas de libre configuración/python/Carlos_Del_Marmol_Fijo_Practica_1.2.py
+++
@@ -1,9 +0,0 @@
-# Generaremos una función que reciba por parámetros, el nombre y apellidos de un alumno 
-# (2 parámetros). Dicha función devolverá la cadena Apellidos, Nombre.
-
-# Imprimiremos dicho resultado por pantalla.
-
-def nombre_completo(nombre, apellido):
-    print(f'El nombre es {nombre} y el apellido es {apellido}')
-
-nombre_completo('Carlos', 'Marmol')
