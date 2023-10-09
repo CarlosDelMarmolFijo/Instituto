6b5ab693202f9999750bea3fff8af0f6eeb48bb6
CarlosDelMarmolFijo <93593998+CarlosDelMarmolFijo@users.noreply.github.com>
2023-10-09 17:22:47 +0200

    Add files via upload

---
+++ Horas de libre configuración/python/Carlos_Del_Marmol_Fijo_Practica_1.3.py
@@ -0,0 +1,27 @@
+# Dado un array tal como [15, 62, 28, 32, 44, 6], haz un archivo que realice las siguientes operaciones:
+
+# Borrar el valor menor sin usar la función min
+# Calcula el promedio del array sin usan la función promedio
+# Añade ese valor en la primera posición del array
+
+array = [15, 62, 28, 32, 44, 6]
+
+"""borra el valor menor"""
+array.pop() 
+
+"""muestra el contenido una vez borradoo el valor"""
+print(array)
+
+"""calcular el valor promedio del array"""
+suma = 0
+for x in array:
+    suma += x
+    
+promedio = suma / len(array)
+
+print(promedio)
+
+"""añadir un valor a la ultima posicion del array"""
+array.insert(6, promedio)
+
+print(array)
