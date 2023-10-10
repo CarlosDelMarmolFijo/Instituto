e545fcd7002c08227fb4d6a5b3fca96f3d2515ee
CarlosDelMarmolFijo <93593998+CarlosDelMarmolFijo@users.noreply.github.com>
2023-10-10 09:25:55 +0200

    PSP_UA1_Tarea1_Carlos_DelMarmol.c

--- Programas C/Ejercicios Clase/Ej01/PSP_UA1_Tarea1_Carlos_DelMarmol.c
+++ Programas C/Ejercicios Clase/Ej01/PSP_UA1_Tarea1_Carlos_DelMarmol.c
@@ -9,20 +9,20 @@ int main()
 	int valor;
 	
 	printf("Introduce un valor entero: ");
-	scanf("%d", &valor);
+	scanf("%d", &valor); // almaceno el valor introducido en la variable "valor"
 	
-	pid = fork();
+	pid = fork(); // pongo en marchar un nuevo proceso con fork
 	
-	if (pid < 0) {
-		printf("No se a podido crear el hijo\n");
-		return(1);
-	} else if (pid == 0) {
+	if (pid < 0) { // si el valor del pid es menor a 0
+		printf("No se a podido crear el hijo\n"); //muestro este mensaje
+		return(1); // y devolvemos 1
+	} else if (pid == 0) { // en caso de ser igual a 0
 		printf("El valor del proceso hijo original es %d\n"),
-		valor += 4;
+		valor += 4; // sumo 4 al valor inicial
 		printf("El valor del proceso hijo despues de sumar 4 es %d\n", valor);
-	} else {
+	} else { 
 		printf("El valor del proceso padre original es %d\n"),
-		valor -= 5;
+		valor -= 5; // sino se cumplen ninguna de las anteriores resto 5 al valor inicial
 		printf("El valor del proceso padre despues de restar 5 es %d\n", valor);
 	}
 	
