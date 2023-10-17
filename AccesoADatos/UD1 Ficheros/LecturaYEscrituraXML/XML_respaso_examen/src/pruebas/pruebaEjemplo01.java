e0e11800f88e5a8aadc36b593e279bef0093582c
CarlosDelMarmolFijo <93593998+CarlosDelMarmolFijo@users.noreply.github.com>
2023-10-17 21:23:28 +0200

    pruebaEjemplo01.java

--- AccesoADatos/UD1 Ficheros/LecturaYEscrituraXML/XML_respaso_examen/src/pruebas/pruebaEjemplo01.java
+++ AccesoADatos/UD1 Ficheros/LecturaYEscrituraXML/XML_respaso_examen/src/pruebas/pruebaEjemplo01.java
@@ -9,7 +9,6 @@ import modelo.xmlDAM;
 public class pruebaEjemplo01 {
 	
 	public static void main(String[] args) {
-		
 		Path p = Paths.get("C:\\Users\\Usuario\\eclipse-workspace\\XML_respaso_examen\\src\\fichero\\ejemplo01.xml");
 		CRUDXMLPrueba02 crud = new CRUDXMLPrueba02(p);
 		xmlDAM dam = crud.leer();
